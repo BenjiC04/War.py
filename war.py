e9dce8a002b03832a546287d3d2653aa5925e817
Ben Clawson <113128420+BenjiC04@users.noreply.github.com>
2022-12-08 09:57:26 -0800

    Add files via upload

--- war.py
+++ war.py
@@ -1,16 +1,15 @@
 #imports
 import turtle as trtl
+import random
 
 #setup
 drawer = trtl.Turtle()
 drawer2 = trtl.Turtle()
+drawer3 = trtl.Turtle()
+drawer4 = trtl.Turtle()
 wn = trtl.Screen()
 
-'''two_spades = "2_of_spades.gif"
-wn.setup(width=1.0, height=1.0)
-drawer.shapesize(0.1, 0.1, 1)
-wn.addshape(two_spades)
-drawer.shape(two_spades)'''
+
 
 #welcome screen
 #background setup
@@ -35,10 +34,56 @@ drawer2.penup()
 drawer2.shape(back_card)
 drawer2.goto(250, 120)
 
+two_spades = "2_of_spades.gif"
+wn.addshape(two_spades)
+
+three_spades = "3_of_spades.gif"
+wn.addshape(three_spades)
+
+four_spades = "4_of_spades.gif"
+wn.addshape(four_spades)
+
+five_spades = "5_of_spades.gif"
+wn.addshape(five_spades)
+
+six_spades = "6_of_spades.gif"
+wn.addshape(six_spades)
+
+seven_spades = "7_of_spades.gif"
+wn.addshape(seven_spades)
+
+eight_spades = "8_of_spades.gif"
+wn.addshape(eight_spades)
+
+nine_spades = "9_of_spades.gif"
+wn.addshape(nine_spades)
+
+ten_spades = "10_of_spades.gif"
+wn.addshape(ten_spades)
+
+jack_spades = "jack_of_spades2.gif"
+wn.addshape(jack_spades)
+
+queen_spades = "queen_of_spades2.gif"
+wn.addshape(queen_spades)
+
+king_spades = "king_of_spades2.gif"
+wn.addshape(king_spades)
+
+ace_spades = "ace_of_spades.gif"
+wn.addshape(ace_spades)
+
+#deal cards
+ranks = [two_spades, three_spades, four_spades]
+card_num = [2, 3, 4]
+
+ #get integers to equal card (2 = two_spades, etc.)
+
+card_num = ranks
+print(card_num)
 
 
-
-
+#deal function
 
 
 
